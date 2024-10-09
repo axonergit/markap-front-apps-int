afb12c32fba0837d6e026c169aca16d704ec0293
Sebyex <sebyex304@gmail.com>
2024-10-08 22:25:28 -0300

    Agregado tailwind css y se agregaron tarjetas de ejemplo de windUI

--- markap/src/App.jsx
+++ markap/src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import NavbarIcons from "./Components/Navbar"
+import CardEcommerce from "./Components/Card"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavbarIcons></NavbarIcons>
+      <div className="grid sm:grid-cols-2 justify-evenly gap-32 py-32 px-11 w-full h-full">
+        <CardEcommerce titulo={"Hola"} precio={8.99} descripcion={"Una espada de oro"}></CardEcommerce>
+        <CardEcommerce></CardEcommerce>
+        <CardEcommerce></CardEcommerce>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
