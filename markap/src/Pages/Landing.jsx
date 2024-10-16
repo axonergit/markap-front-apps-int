b922e6e50db196c7fb9c86fb5971ebaa253b5381
Sebastian Z. <sebyex304@gmail.com>
2024-10-15 23:16:22 -0300

    Agregada la primera parte del registro (ya se pueden recolectar los datos y guardarlos en un objeto, tendriamos que mandarlos al backend.)

--- markap/src/Pages/Landing.jsx
+++ markap/src/Pages/Landing.jsx
@@ -1,9 +1,19 @@
 import MyNavbar from "../Navbar.jsx";
+import {Button, ButtonGroup} from "@nextui-org/button";
+
 
 const Landing = () => {
     return (
-
+        <>
         <MyNavbar></MyNavbar>
+            <div className="w-screen h-screen bg-gradient-to-l from-blue-950 to-blue-500 grid p-4 gap-4 grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:grid-cols-4">
+                <div className="w-32 h-32 bg-white"></div>
+                <div className="w-32 h-32 bg-red-400"></div>
+                <div className="w-32 h-32 bg-amber-400"></div>
+                <div className="w-32 h-32 bg-green-400"></div>
+
+            </div>
+        </>
     )
 }
 
--- markap/src/Pages/Register.jsx
+++ markap/src/Pages/Register.jsx
@@ -1,7 +1,74 @@
+import {useForm} from "react-hook-form";
+import {DatePicker, Input,Button} from "@nextui-org/react";
+
 const Register = () => {
+    const {register,setValue ,handleSubmit, watch, formState: {errors}} = useForm();
+    const handleDateChange = (date) => {
+        setValue("birthDate",date)
+    }
+
+
+    const onSubmit = data => console.log(data);
 
     return (
-        <h1>Hola estamos en el register</h1>
+        <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-b from-blue-950 to-blue-500">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-5/6 bg-white p-8 rounded-2xl gap-8 md:w-2/3 lg:w-5/12 sm:p-14">
+                <div>
+                    <Input
+                        {...register("username", {required: true})}
+                        label={"Nombre de usuario"}
+                        placeholder="Ingrese su nombre de usuario"
+                        isRequired={true}
+                    />
+                    {errors.username && (<span>This field is required</span>)}
+                </div>
+
+                <div>
+                    <Input
+                        {...register("password", {required: true})}
+                        label={"Contraseña"}
+                        placeholder="Ingrese su contraseña"
+                        type={"password"}
+                        isRequired={true}
+                    />
+                    {errors.password && (<span>This field is required</span>)}
+                </div>
+                <div>
+                    <Input
+                        {...register("name", {required: true})}
+                        label={"Nombre"}
+                        placeholder="Ingrese su contraseña"
+                        isRequired={true}
+                    />
+                    {errors.name && (<span>This field is required</span>)}
+                </div>
+                <div>
+                    <Input
+                        {...register("lastName", {required: true})}
+                        label={"Apellido"}
+                        placeholder="Ingrese su contraseña"
+                        isRequired={true}
+                    />
+                    {errors.lastName && (<span>This field is required</span>)}
+                </div>
+                <div>
+                    <Input
+                        {...register("email", {required: true})}
+                        label={"Correo electronico"}
+                        placeholder="Ingrese su correo electronico"
+                        type={"email"}
+                        isRequired={true}
+
+                    />
+                    {errors.email && (<span>This field is required</span>)}
+                </div>
+
+                <DatePicker label="Fecha de nacimiento" onChange={handleDateChange}/>
+                <Button type="submit" color="primary">Registrarse</Button>
+
+            </form>
+
+        </div>
     )
 }
 
