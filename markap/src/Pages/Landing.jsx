93e452decfa4e1ac38f4d828eebdf4f078e0c8ef
SorongoTongo <alejoperve@gmail.com>
2024-10-18 17:12:50 -0300

    estructura básica de la Landing Page, galta productos visitados

--- markap/src/Pages/Landing.jsx
+++ markap/src/Pages/Landing.jsx
@@ -1,18 +1,57 @@
 import MyNavbar from "../Components/Navbar.jsx";
 import {Button, ButtonGroup} from "@nextui-org/button";
+import axiosClient from "../config/axiosClient.js";
+import {useState} from "react";
+import Card from "../Components/Card.jsx";
+import Destacados from "../Components/Destacados.jsx";
+import Categorias from "../Components/Categorias.jsx";
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 
 const Landing = () => {
+    const token = localStorage.getItem("authToken");
+
+    const { isLoading, error, data } = useQuery({   //Usamos useQuery con la ruta del backend (tomamos los destacados)
+        queryKey: ['Categorias'],
+        queryFn: () =>
+          axiosClient.get("http://localhost:3000/categorias").then((res) => res.data),
+      });
+    
+      if (isLoading) return 'Loading...';
+    
+      console.log(data)
+
     return (
         <>
         <MyNavbar></MyNavbar>
-            <div className="w-screen h-screen bg-gradient-to-l from-blue-950 to-blue-500 grid p-4 gap-4 grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:grid-cols-4">
-                <div className="w-32 h-32 bg-white"></div>
-                <div className="w-32 h-32 bg-red-400"></div>
-                <div className="w-32 h-32 bg-amber-400"></div>
-                <div className="w-32 h-32 bg-green-400"></div>
+            <div className="w-screen h-screen bg-gradient-to-l from-blue-950 to-blue-500 flex justify-center items-center">
+                <Destacados>
+            
+                </Destacados>
+            </div>
+
+            <div className="w-screen h-screen bg-gradient-to-l from-black to-red-800 flex justify-center items-center">
+
+                <Categorias data = {data}>
+
+                
+
+                </Categorias>
 
             </div>
+
+            <div className="w-screen h-screen bg-gradient-to-l from-black to-red-800 flex justify-center items-center">
+
+
+
+
+
+            </div>
+
+            
+
+            {token ? <h1>Logeado gil</h1> : <h1>no logeado gil</h1>} { /*<!--esto junto a const token nos sirven para renderizar el historial de productos visitados del usuario cuando se loguea -->*/}
         </>
     )
 }
