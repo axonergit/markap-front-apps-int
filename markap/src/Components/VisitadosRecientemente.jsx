2094c14d62981054b31e6396bc5b2b334a2d21ef
SorongoTongo <alejoperve@gmail.com>
2024-11-09 19:13:31 -0300

    Arreglos menores

--- markap/src/Components/VisitadosRecientemente.jsx
+++ markap/src/Components/VisitadosRecientemente.jsx
@@ -5,6 +5,8 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// usar UseContext para mostrar algun texto como "Tenes que estar logueado para ver productos recientemente visitados"
+
 export default function VisitadosRecientemente() {
     const settings = {
       dots: true,
@@ -45,17 +47,42 @@ export default function VisitadosRecientemente() {
     if (error) return <div>Error cargando productos visitados recientemente</div>;
   
     return (
-      <div className="container mx-auto px-4 py-5">
-        <Slider {...settings}>
-          {data?.map((producto) => (
-            <div key={producto.productEntity.id}>
-              <img src={producto.productEntity.imagen} alt="Producto"/>
-              <h2>{producto.productEntity.descripcion}</h2>
-              <p>Precio: ${producto.productEntity.precio}</p>
-            </div>
-          ))}
-        </Slider>
+<>
+    <div className="w-full h-1/2 flex items-center justify-center py-14">
+      <div className="w-full max-w-6xl bg-white p-6 rounded-lg shadow-lg py-5">
+        <p className="text-lg mb-4">{"Productos de la categoría"}</p>
 
+        {data.content && data.content.length > 0 ? (
+          <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+            {data.content.slice(0, 5).map((producto) => (
+              <Link
+                to={`/producto/${producto.id}`}
+                key={producto.id}
+                className="bg-gray-100 p-4 rounded-lg shadow-md h-full flex flex-col items-center justify-between"
+              >
+                <h2 className="text-lg font-semibold text-center mb-2 truncate-multiline">
+                  {producto.descripcion}
+                </h2>
+                <img
+                  src={producto.imagen}
+                  className="w-full h-48 object-contain mb-3"
+                  alt="Producto"
+                />
+                <p className="text-sm text-gray-600 mb-2 text-center h-10 overflow-hidden">
+                  {producto.detalles}
+                </p>
+                <p className="font-bold text-green-500 text-lg">
+                  Precio: ${producto.precio.toFixed(2)}
+                </p>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p>No hay productos en esta categoría.</p>
+        )}
       </div>
+    </div>
+</>
+
     );
 }
