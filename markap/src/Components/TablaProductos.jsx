70a8120cf80610e4387b18ae266126d45395b920
Fran <francelee10@gmail.com>
2024-11-11 23:46:05 -0300

    Elimine destacados en TablaProductos

--- markap/src/Components/TablaProductos.jsx
+++ markap/src/Components/TablaProductos.jsx
@@ -1,24 +1,28 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axiosClient from "../config/axiosClient.js";
 import ModificarProducto from "./ModificarProducto.jsx";
 import { Button, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, Card, CardBody, CardHeader, CardFooter } from "@nextui-org/react";
 import CrearProducto from "./CrearProducto.jsx";
+import { toast } from "react-hot-toast";
 
 const TablaProductos = () => {
     const queryClient = useQueryClient();
 
-    const { isLoading, error, data } = useQuery({
+    const { isLoading, error, data: productos } = useQuery({
         queryKey: ['Productos'],
         queryFn: () => axiosClient.get("/productos").then((res) => res.data),
     });
+
 
     const EliminarProducto = async (id) => {
         if (confirm("¿Estás seguro de que quieres eliminar este producto?")) {
             try {
                 await axiosClient.delete(`/productos/${id}`);
                 queryClient.invalidateQueries(['Productos']);
+                toast.success('Producto eliminado exitosamente');
             } catch (error) {
                 console.error("Error al eliminar el producto:", error);
+                toast.error("Error al eliminar el producto");
             }
         }
     }
@@ -37,14 +41,14 @@ const TablaProductos = () => {
                     <TableHeader>
                         <TableColumn>ID</TableColumn>
                         <TableColumn>Nombre producto</TableColumn>
-                        <TableColumn hideHeader={true}>Descripción</TableColumn>
+                        <TableColumn>Descripción</TableColumn>
                         <TableColumn>Categoría</TableColumn>
                         <TableColumn>Precio</TableColumn>
                         <TableColumn>Stock</TableColumn>
                         <TableColumn>Acciones</TableColumn>
                     </TableHeader>
                     <TableBody emptyContent="No hay productos para mostrar.">
-                        {data?.map((producto) => (
+                        {productos?.map((producto) => (
                             <TableRow key={producto.id}>
                                 <TableCell>{producto.id}</TableCell>
                                 <TableCell>{producto.descripcion}</TableCell>
@@ -77,7 +81,7 @@ const TablaProductos = () => {
             </CardBody>
             <CardFooter>
                 <div className="flex justify-between w-full">
-                    <p>Total de productos: {data?.length || 0}</p>
+                    <p>Total de productos: {productos?.length || 0}</p>
                 </div>
             </CardFooter>
         </Card>
