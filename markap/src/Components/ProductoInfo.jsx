60a5e9878a781e866faa6bdade3ae140e3747752
mathiuSpv <mathius998@gmail.com>
2024-11-11 22:41:40 -0300

    no es mucho pero es trabajo digno

--- markap/src/Components/ProductoInfo.jsx
+++ markap/src/Components/ProductoInfo.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import {Chip} from "@nextui-org/react";
+import {Chip , Link} from "@nextui-org/react";
+import {useQuery} from "@tanstack/react-query";
+import axiosClient from "../config/axiosClient.js";
 
 // eslint-disable-next-line react/prop-types
 export default function ProductoInfo( { productoJson } ) {
+
+    const {data: CategoriaProducto, isLoading, error} = useQuery({
+        queryKey: ['Categoria'],
+        queryFn: async () => {
+            const response = await axiosClient.get("productos/categoria");
+            return response.data;
+        },
+        enabled: !!productoJson
+    })
+
+    const matchedCategoria = CategoriaProducto?.find(
+        (categoria) => categoria.nombreCategoria === productoJson.nombreCategoria
+    );
+
     return (
-        <>
+        <div>
             <h1 style={{fontSize: "2.5rem", fontWeight: "bold"}}>{productoJson.descripcion}</h1>
             <p style={{width: '25rem'}}>
                 {productoJson.detalles}
             </p>
-            <h2> $ {productoJson.precio}</h2>
-            <h2>Categoria: <Chip>{productoJson.nombreCategoria}</Chip></h2>
-
-        </>
+            <h2 style={{fontSize: "1.6rem", fontWeight: "bold"}}> $ {productoJson.precio}</h2>
+            <h2 style={{fontSize: "1rem", fontWeight: "bold"}}>
+                Categoria: <Chip>
+                <Link href={CategoriaProducto && "../productos/categoria/"+matchedCategoria.id}>
+                {productoJson.nombreCategoria}
+                </Link>
+                </Chip>
+            </h2>
+            <h6 style={{fontSize: ".8rem", fontWeight: "bold"}}>Vendedor: <Link>{productoJson.nombreUserVendedor}</Link></h6>
+        </div>
     )
 }
--- markap/src/Pages/ProductoVista.jsx
+++ markap/src/Pages/ProductoVista.jsx
@@ -4,27 +4,52 @@ import axiosClient from "../config/axiosClient.js";
 import ProductoImage from "../Components/ProductoImage.jsx";
 import { useParams } from "react-router-dom";
 import ProductoInfo from "../Components/ProductoInfo.jsx";
+import ProductoAgregar from "../Components/ProductoAgregar.jsx";
+import {useEffect, useState} from "react";
+import ProductoComprar from "../Components/ProductoComprar.jsx";
 
 const ProductoVista = () => {
 
     const { productId } = useParams();
+    const [cantidadActual, setCantidadActual] = useState(0);
+    const [cantidad, setCantidad] = useState(0);
     const {data: ProductoResponse, isLoading, error} = useQuery({
         queryKey: ['Producto', productId],
         queryFn: async () => {
             const response = await axiosClient.get(`/productos/${productId}`);
-            console.log(productId, response.data);
             return response.data;
         },
         staleTime: 1000 * 60 * 2,
     })
 
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axiosClient.get('/carrito/actual/items');
+            const datos = response.data.content;
+            const productoResultado = datos?.find(
+                (productos) => productos.product.id == productId
+            );
+            if (productoResultado) setCantidadActual(productoResultado.amount)
+            console.log(productoResultado.amount)
+        }
+        fetchData();
+    }, [])
+
+
     return (
         <div className="flex flex-col">
             <MyNavbar/>
-            <div className="flex flex-row">
+            <div className="flex flex-row" style={{
+                marginTop: "1rem",
+                marginLeft: "12rem",
+                gap: "2rem"
+            }}>
                 {ProductoResponse && <ProductoImage productoJson={ProductoResponse}/>}
-                <div style={{display: "flex", flexGrow: "1", flexDirection: "column"}}>
+                <div style={{display: "flex", flexGrow: "1", flexDirection: "column", gap: "2vh"}}>
                     {ProductoResponse && <ProductoInfo productoJson={ProductoResponse}/>}
+                    {ProductoResponse && <ProductoAgregar productoJson={ProductoResponse}
+                      cantidad={cantidad} cantidadQuery={cantidadActual} setCantidad={setCantidad}/>}
+                    {ProductoResponse && <ProductoComprar productoJson={ProductoResponse} cantidad={cantidad}/>}
                 </div>
             </div>
         </div>
