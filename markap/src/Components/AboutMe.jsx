5f50868e6f93f1b0ee890d000f228baa427f1d41
Sebastian Z. <sebyex304@gmail.com>
2024-11-12 00:09:52 -0300

    Cambiado el nombre de la pagina

--- markap/src/Components/AboutMe.jsx
+++ markap/src/Components/AboutMe.jsx
@@ -28,7 +28,7 @@ export default function AboutMe(){
 
     if (userProfile) return (
         <div className="about-me">
-            <Avatar src="" className="w-80 h-80 text-large"/>
+            <Avatar src="https://akamai.sscdn.co/uploadfile/letras/fotos/5/9/1/b/591b070413cf1d9c34bc8528ee500268.jpg" className="w-80 h-80 text-large"/>
             <Accordion>
                 <AccordionItem className="w-80 h-190" key="1" subtitle="Ver Informacion"
                                title={userProfile.nombreCompleto}>
