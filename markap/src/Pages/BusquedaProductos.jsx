2922bbf8d50a729c68fb2089535445522f7b728a
SorongoTongo <alejoperve@gmail.com>
2024-11-24 12:25:17 -0300

    Ajuste de estilo

--- markap/src/Pages/BusquedaProductos.jsx
+++ markap/src/Pages/BusquedaProductos.jsx
@@ -36,49 +36,59 @@ const BusquedaProductos = () => {
   }
 
   return (
+    <>
     <div>
       <MyNavbar />
-      <div className="w-full min-h-screen bg-gradient-to-r from-slate-900 to-slate-800 flex items-center justify-center py-14">
+      <div className="w-full min-h-screen flex items-center justify-center py-14">
         <div className="w-full max-w-6xl bg-white p-6 rounded-lg shadow-lg py-5">
-          <p className="text-lg mb-4">Resultados para: "{nombre}"</p>
+          <p className="text-lg mb-4">{"Gestion de la categoría"}</p>
 
           {data.content && data.content.length > 0 ? (
-            <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-              {data.content.map((producto) => (
-                <Link
-                  to={`/producto/${producto.id}`}
-                  key={producto.id}
-                  className="bg-gray-100 p-4 rounded-lg shadow-md h-full flex flex-col items-center justify-between"
-                >
-                  <h2 className="text-lg font-semibold text-center mb-2 truncate-multiline">
-                    {producto.descripcion}
-                  </h2>
-                  <img
-                    src={producto.imagen}
-                    className="w-full h-48 object-contain mb-3"
-                    alt="Producto"
-                  />
-                  <p className="text-sm text-gray-600 mb-2 text-center h-10 overflow-hidden">
-                    {producto.detalles}
-                  </p>
-                  <p className="font-bold text-green-500 text-lg">Precio: ${producto.precio.toFixed(2)}</p>
-                </Link>
-              ))}
-            </ul>
-          ) : (
-            <p>No se encontraron productos para "{nombre}".</p>
-          )}
+           <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+           {data.content.map((producto) => (
+               <Link
+                   to={`/producto/${producto.id}`}
+                   key={producto.id}
+               >
+                 <Card
+                     className="border-2 border-base-300 bg-white"
+                     shadow="sm"
+                 >
+                   <CardBody className="overflow-visible p-0">
+                     <Image
+                         shadow="sm"
+                         alt={producto.id}
+                         width="100%"
+                         className="w-full object-contain h-[200px] border-1 border-base-300 bg-white"
+                         src={producto.imagen}
+                     />
+                   </CardBody>
+                   <p className="text-center mt-2">{producto.descripcion}</p>
+                   <CardFooter className="justify-center">
+                     <p className="font-bold">${producto.precio.toFixed(2)}</p>
+                   </CardFooter>
+                 </Card>
+               </Link>
+           ))}
+           </ul>
 
+          ) : (
+            <p>No hay productos en esta categoría.</p>
+          )}
+          
           <div className="py-10">
-            <Pagination
-              currentPage={currentPage + 1}
+            <Pagination 
+              categoryId={id} 
+              currentPage={currentPage + 1} 
               onPageChange={(page) => setCurrentPage(page - 1)}
               totalPages={data.totalPages}
             />
           </div>
+          
         </div>
       </div>
     </div>
+    </>
   );
 };
 
